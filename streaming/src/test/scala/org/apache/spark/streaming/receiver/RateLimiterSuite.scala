9dd8dcdda9624a70c98fdc9d3523e1fcfe65d009
Liwei Lin <proflin.me@gmail.com>
2016-02-29 16:43:19 +0800

    add test cases for rate limiter

--- streaming/src/test/scala/org/apache/spark/streaming/receiver/RateLimiterSuite.scala
+++ streaming/src/test/scala/org/apache/spark/streaming/receiver/RateLimiterSuite.scala
@@ -17,8 +17,7 @@
 
 package org.apache.spark.streaming.receiver
 
-import org.apache.spark.SparkConf
-import org.apache.spark.SparkFunSuite
+import org.apache.spark.{SparkConf, SparkFunSuite}
 
 /** Testsuite for testing the network receiver behavior */
 class RateLimiterSuite extends SparkFunSuite {
@@ -94,5 +93,33 @@ class RateLimiterSuite extends SparkFunSuite {
     assert(rateLimiter.rateLimitHistory.length == 1)
     assert(rateLimiter.rateLimitHistory(0).limit == 50)
     assert(rateLimiter.rateLimitHistory(0).ts == 2500)
+  }
+
+  test("sumRateLimits() works good for normal cases") {
+    // if rateLimits contains some None, then sumRateLimits() should return None
+    var rateLimits: Seq[Option[Long]] = Seq(Some(1L), None)
+    assert(RateLimiterHelper.sumRateLimits(rateLimits) == None)
+
+    // normal case
+    rateLimits = Seq(Some(10L))
+    var sum: Option[Long] = RateLimiterHelper.sumRateLimits(rateLimits)
+    assert(sum.isDefined && sum.get == 10L)
+    
+    // another normal case
+    rateLimits = Seq(Some(10L), Some(20L), Some(30L), Some(40L))
+    sum = RateLimiterHelper.sumRateLimits(rateLimits)
+    assert(sum.isDefined && sum.get == 100L)
+  }
+
+  test("sumRateLimits() works good for overflow cases") {
+    // overflow case
+    var rateLimits: Seq[Option[Long]] = Seq(Some(Long.MaxValue), Some(1L))
+    var sum = RateLimiterHelper.sumRateLimits(rateLimits)
+    assert(sum.isDefined && sum.get == Long.MaxValue)
+
+    // another overflow case
+    rateLimits = Seq(Some(Long.MaxValue), Some(Long.MaxValue), Some(Long.MaxValue))
+    sum = RateLimiterHelper.sumRateLimits(rateLimits)
+    assert(sum.isDefined && sum.get == Long.MaxValue)
   }
 }
