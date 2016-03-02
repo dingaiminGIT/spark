8ea698d7533fc964062d0b6a44338c1f28a5c3de
proflin <proflin.me@gmail.com>
2016-03-02 20:40:23 +0800

    Adds in `RateLimiter` a data structure keeping history of rate limit changes

--- streaming/src/main/scala/org/apache/spark/streaming/receiver/RateLimiter.scala
+++ streaming/src/main/scala/org/apache/spark/streaming/receiver/RateLimiter.scala
@@ -17,6 +17,8 @@
 
 package org.apache.spark.streaming.receiver
 
+import scala.collection.mutable.ArrayBuffer
+
 import com.google.common.util.concurrent.{RateLimiter => GuavaRateLimiter}
 
 import org.apache.spark.{Logging, SparkConf}
@@ -57,8 +59,10 @@ private[receiver] abstract class RateLimiter(conf: SparkConf) extends Logging {
     if (newRate > 0) {
       if (maxRateLimit > 0) {
         rateLimiter.setRate(newRate.min(maxRateLimit))
+        appendLimitToHistory(newRate.min(maxRateLimit))
       } else {
         rateLimiter.setRate(newRate)
+        appendLimitToHistory(newRate)
       }
     }
 
@@ -67,5 +71,60 @@ private[receiver] abstract class RateLimiter(conf: SparkConf) extends Logging {
    */
   private def getInitialRateLimit(): Long = {
     math.min(conf.getLong("spark.streaming.backpressure.initialRate", maxRateLimit), maxRateLimit)
+  }
+
+  private[receiver] case class RateLimitSnapshot(limit: Double, ts: Long)
+
+  private[receiver] val rateLimitHistory: ArrayBuffer[RateLimitSnapshot] =
+    ArrayBuffer(RateLimitSnapshot(getInitialRateLimit().toDouble, -1L))
+
+  /**
+   * Logs the rateLimit change history, so that we can do a sum later.
+   *
+   * @param rate the new rate
+   * @param ts at which time the rate changed
+   */
+  private[receiver] def appendLimitToHistory(rate: Double, ts: Long = System.currentTimeMillis()) {
+    rateLimitHistory.synchronized {
+      rateLimitHistory += RateLimitSnapshot(rate, ts)
+    }
+  }
+
+  private val blockIntervalMs = conf.getTimeAsMs("spark.streaming.blockInterval", "200ms")
+  require(blockIntervalMs > 0, s"'spark.streaming.blockInterval' should be a positive value")
+
+  /**
+   * Calculate the upper bound of how many events can be received in a block interval.
+   * Note this should be called for each block interval once and only once.
+   *
+   * @param ts the ending timestamp of a block interval
+   * @return the upper bound of how many events can be received in a block interval
+   */
+  private[receiver]  def sumHistoryThenTrim(ts: Long = System.currentTimeMillis()): Long = {
+    var sum: Double = 0
+    rateLimitHistory.synchronized {
+      // first add a RateLimitSnapshot
+      // this RateLimitSnapshot will be used as the ending of this block interval and the beginning
+      // of the next block interval
+      rateLimitHistory += RateLimitSnapshot(rateLimitHistory.last.limit, ts)
+
+      // then do a sum
+      for (idx <- 0 until rateLimitHistory.length - 1) {
+        val duration = rateLimitHistory(idx + 1).ts - (if (rateLimitHistory(idx).ts < 0) {
+          rateLimitHistory.last.ts - blockIntervalMs
+        }
+        else {
+          rateLimitHistory(idx).ts
+        })
+        sum += rateLimitHistory(idx).limit * duration
+      }
+
+      // trim the history to the last one
+      rateLimitHistory.trimStart(rateLimitHistory.length - 1)
+    }
+
+    // 10.0 => 10
+    // 10.1 => 11
+    (sum / 1000 + 0.999).toLong
   }
 }
