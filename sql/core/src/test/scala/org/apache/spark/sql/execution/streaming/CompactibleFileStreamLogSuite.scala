060d6ba0270dd1ac6445736cd68559f2d05b220c
Liwei Lin <lwlin7@gmail.com>
2016-11-09 14:40:18 +0800

    One by One

--- sql/core/src/test/scala/org/apache/spark/sql/execution/streaming/CompactibleFileStreamLogSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/streaming/CompactibleFileStreamLogSuite.scala
@@ -103,207 +103,107 @@ class CompactibleFileStreamLogSuite extends SparkFunSuite with SharedSQLContext 
     assert(Seq(8) === getAllValidBatches(8, compactInterval = 3))
   }
 
-/*
-  test("compactLogs") {
-    withFileStreamSinkLog { sinkLog =>
-      val logs = Seq(
-        newFakeSinkFileStatus("/a/b/x", FileStreamSinkLog.ADD_ACTION),
-        newFakeSinkFileStatus("/a/b/y", FileStreamSinkLog.ADD_ACTION),
-        newFakeSinkFileStatus("/a/b/z", FileStreamSinkLog.ADD_ACTION))
-      assert(logs === sinkLog.compactLogs(logs))
-
-      val logs2 = Seq(
-        newFakeSinkFileStatus("/a/b/m", FileStreamSinkLog.ADD_ACTION),
-        newFakeSinkFileStatus("/a/b/n", FileStreamSinkLog.ADD_ACTION),
-        newFakeSinkFileStatus("/a/b/z", FileStreamSinkLog.DELETE_ACTION))
-      assert(logs.dropRight(1) ++ logs2.dropRight(1) === sinkLog.compactLogs(logs ++ logs2))
-    }
-  }
-
-  test("serialize") {
-    withFileStreamSinkLog { sinkLog =>
-      val logs = Array(
-        SinkFileStatus(
-          path = "/a/b/x",
-          size = 100L,
-          isDir = false,
-          modificationTime = 1000L,
-          blockReplication = 1,
-          blockSize = 10000L,
-          action = FileStreamSinkLog.ADD_ACTION),
-        SinkFileStatus(
-          path = "/a/b/y",
-          size = 200L,
-          isDir = false,
-          modificationTime = 2000L,
-          blockReplication = 2,
-          blockSize = 20000L,
-          action = FileStreamSinkLog.DELETE_ACTION),
-        SinkFileStatus(
-          path = "/a/b/z",
-          size = 300L,
-          isDir = false,
-          modificationTime = 3000L,
-          blockReplication = 3,
-          blockSize = 30000L,
-          action = FileStreamSinkLog.ADD_ACTION))
-
-      // scalastyle:off
-      val expected = s"""$VERSION
-          |{"path":"/a/b/x","size":100,"isDir":false,"modificationTime":1000,"blockReplication":1,"blockSize":10000,"action":"add"}
-          |{"path":"/a/b/y","size":200,"isDir":false,"modificationTime":2000,"blockReplication":2,"blockSize":20000,"action":"delete"}
-          |{"path":"/a/b/z","size":300,"isDir":false,"modificationTime":3000,"blockReplication":3,"blockSize":30000,"action":"add"}""".stripMargin
-      // scalastyle:on
-      val baos = new ByteArrayOutputStream()
-      sinkLog.serialize(logs, baos)
-      assert(expected === baos.toString(UTF_8.name()))
-      baos.reset()
-      sinkLog.serialize(Array(), baos)
-      assert(VERSION === baos.toString(UTF_8.name()))
-    }
-  }
-
-  test("deserialize") {
-    withFileStreamSinkLog { sinkLog =>
-      // scalastyle:off
-      val logs = s"""$VERSION
-          |{"path":"/a/b/x","size":100,"isDir":false,"modificationTime":1000,"blockReplication":1,"blockSize":10000,"action":"add"}
-          |{"path":"/a/b/y","size":200,"isDir":false,"modificationTime":2000,"blockReplication":2,"blockSize":20000,"action":"delete"}
-          |{"path":"/a/b/z","size":300,"isDir":false,"modificationTime":3000,"blockReplication":3,"blockSize":30000,"action":"add"}""".stripMargin
-      // scalastyle:on
-
-      val expected = Seq(
-        SinkFileStatus(
-          path = "/a/b/x",
-          size = 100L,
-          isDir = false,
-          modificationTime = 1000L,
-          blockReplication = 1,
-          blockSize = 10000L,
-          action = FileStreamSinkLog.ADD_ACTION),
-        SinkFileStatus(
-          path = "/a/b/y",
-          size = 200L,
-          isDir = false,
-          modificationTime = 2000L,
-          blockReplication = 2,
-          blockSize = 20000L,
-          action = FileStreamSinkLog.DELETE_ACTION),
-        SinkFileStatus(
-          path = "/a/b/z",
-          size = 300L,
-          isDir = false,
-          modificationTime = 3000L,
-          blockReplication = 3,
-          blockSize = 30000L,
-          action = FileStreamSinkLog.ADD_ACTION))
-
-      assert(expected === sinkLog.deserialize(new ByteArrayInputStream(logs.getBytes(UTF_8))))
-
-      assert(Nil === sinkLog.deserialize(new ByteArrayInputStream(VERSION.getBytes(UTF_8))))
-    }
-  }
-
   test("batchIdToPath") {
     withSQLConf(SQLConf.FILE_SINK_LOG_COMPACT_INTERVAL.key -> "3") {
-      withFileStreamSinkLog { sinkLog =>
-        assert("0" === sinkLog.batchIdToPath(0).getName)
-        assert("1" === sinkLog.batchIdToPath(1).getName)
-        assert("2.compact" === sinkLog.batchIdToPath(2).getName)
-        assert("3" === sinkLog.batchIdToPath(3).getName)
-        assert("4" === sinkLog.batchIdToPath(4).getName)
-        assert("5.compact" === sinkLog.batchIdToPath(5).getName)
+      withFakeCompactibleFileStreamLog { compactibleLog =>
+        assert("0" === compactibleLog.batchIdToPath(0).getName)
+        assert("1" === compactibleLog.batchIdToPath(1).getName)
+        assert("2.compact" === compactibleLog.batchIdToPath(2).getName)
+        assert("3" === compactibleLog.batchIdToPath(3).getName)
+        assert("4" === compactibleLog.batchIdToPath(4).getName)
+        assert("5.compact" === compactibleLog.batchIdToPath(5).getName)
       }
     }
   }
+
 
   testWithUninterruptibleThread("compact") {
     withSQLConf(SQLConf.FILE_SINK_LOG_COMPACT_INTERVAL.key -> "3") {
-      withFileStreamSinkLog { sinkLog =>
+      withFakeCompactibleFileStreamLog { compactibleLog =>
         for (batchId <- 0 to 10) {
-          sinkLog.add(
+          compactibleLog.add(
             batchId,
-            Array(newFakeSinkFileStatus("/a/b/" + batchId, FileStreamSinkLog.ADD_ACTION)))
+            Array("some_path_"+batchId))
           val expectedFiles = (0 to batchId).map {
-            id => newFakeSinkFileStatus("/a/b/" + id, FileStreamSinkLog.ADD_ACTION)
+            id => "some_path" + id
           }
-          assert(sinkLog.allFiles() === expectedFiles)
+          assert(compactibleLog.allFiles() === expectedFiles)
           if (isCompactionBatch(batchId, 3)) {
             // Since batchId is a compaction batch, the batch log file should contain all logs
-            assert(sinkLog.get(batchId).getOrElse(Nil) === expectedFiles)
+            assert(compactibleLog.get(batchId).getOrElse(Nil) === expectedFiles)
           }
         }
       }
     }
   }
+  /*
+          testWithUninterruptibleThread("delete expired file") {
+            // Set FILE_SINK_LOG_CLEANUP_DELAY to 0 so that we can detect the deleting behaviour
+            // deterministically
+            withSQLConf(
+              SQLConf.FILE_SINK_LOG_COMPACT_INTERVAL.key -> "3",
+              SQLConf.FILE_SINK_LOG_CLEANUP_DELAY.key -> "0") {
+              withFileStreamSinkLog { sinkLog =>
+                val fs = sinkLog.metadataPath.getFileSystem(spark.sessionState.newHadoopConf())
 
-  testWithUninterruptibleThread("delete expired file") {
-    // Set FILE_SINK_LOG_CLEANUP_DELAY to 0 so that we can detect the deleting behaviour
-    // deterministically
-    withSQLConf(
-      SQLConf.FILE_SINK_LOG_COMPACT_INTERVAL.key -> "3",
-      SQLConf.FILE_SINK_LOG_CLEANUP_DELAY.key -> "0") {
-      withFileStreamSinkLog { sinkLog =>
-        val fs = sinkLog.metadataPath.getFileSystem(spark.sessionState.newHadoopConf())
+                def listBatchFiles(): Set[String] = {
+                  fs.listStatus(sinkLog.metadataPath).map(_.getPath.getName).filter { fileName =>
+                    try {
+                      getBatchIdFromFileName(fileName)
+                      true
+                    } catch {
+                      case _: NumberFormatException => false
+                    }
+                  }.toSet
+                }
 
-        def listBatchFiles(): Set[String] = {
-          fs.listStatus(sinkLog.metadataPath).map(_.getPath.getName).filter { fileName =>
-            try {
-              getBatchIdFromFileName(fileName)
-              true
-            } catch {
-              case _: NumberFormatException => false
+                sinkLog.add(0, Array(newFakeSinkFileStatus("/a/b/0", FileStreamSinkLog.ADD_ACTION)))
+                assert(Set("0") === listBatchFiles())
+                sinkLog.add(1, Array(newFakeSinkFileStatus("/a/b/1", FileStreamSinkLog.ADD_ACTION)))
+                assert(Set("0", "1") === listBatchFiles())
+                sinkLog.add(2, Array(newFakeSinkFileStatus("/a/b/2", FileStreamSinkLog.ADD_ACTION)))
+                assert(Set("2.compact") === listBatchFiles())
+                sinkLog.add(3, Array(newFakeSinkFileStatus("/a/b/3", FileStreamSinkLog.ADD_ACTION)))
+                assert(Set("2.compact", "3") === listBatchFiles())
+                sinkLog.add(4, Array(newFakeSinkFileStatus("/a/b/4", FileStreamSinkLog.ADD_ACTION)))
+                assert(Set("2.compact", "3", "4") === listBatchFiles())
+                sinkLog.add(5, Array(newFakeSinkFileStatus("/a/b/5", FileStreamSinkLog.ADD_ACTION)))
+                assert(Set("5.compact") === listBatchFiles())
+              }
             }
-          }.toSet
-        }
-
-        sinkLog.add(0, Array(newFakeSinkFileStatus("/a/b/0", FileStreamSinkLog.ADD_ACTION)))
-        assert(Set("0") === listBatchFiles())
-        sinkLog.add(1, Array(newFakeSinkFileStatus("/a/b/1", FileStreamSinkLog.ADD_ACTION)))
-        assert(Set("0", "1") === listBatchFiles())
-        sinkLog.add(2, Array(newFakeSinkFileStatus("/a/b/2", FileStreamSinkLog.ADD_ACTION)))
-        assert(Set("2.compact") === listBatchFiles())
-        sinkLog.add(3, Array(newFakeSinkFileStatus("/a/b/3", FileStreamSinkLog.ADD_ACTION)))
-        assert(Set("2.compact", "3") === listBatchFiles())
-        sinkLog.add(4, Array(newFakeSinkFileStatus("/a/b/4", FileStreamSinkLog.ADD_ACTION)))
-        assert(Set("2.compact", "3", "4") === listBatchFiles())
-        sinkLog.add(5, Array(newFakeSinkFileStatus("/a/b/5", FileStreamSinkLog.ADD_ACTION)))
-        assert(Set("5.compact") === listBatchFiles())
-      }
-    }
-  }
+          }
 
 
-  testWithUninterruptibleThread("HDFSMetadataLog: basic") {
-    withTempDir { temp =>
-      val dir = new File(temp, "dir") // use non-existent directory to test whether log make the dir
-      val metadataLog = new FakeCompactibleFileStreamLog("vvv", spark, dir.getAbsolutePath)
-      assert(metadataLog.add(0, Array("batch0")))
-      val x = metadataLog.getLatest();
-      x
-      val y: Tuple2[Int, Int] = Tuple2(1, 2)
-      y.equals(2)
-      assert((1, "b") === (1,"b"))
-      assert(metadataLog.getLatest() === Some(0 -> Array("batch0")))
-      assert(metadataLog.get(0) === Some("batch0"))
-      assert(metadataLog.getLatest() === Some(0 -> "batch0"))
-      assert(metadataLog.get(None, Some(0)) === Array(0 -> "batch0"))
+          testWithUninterruptibleThread("HDFSMetadataLog: basic") {
+            withTempDir { temp =>
+              val dir = new File(temp, "dir") // use non-existent directory to test whether log make the dir
+              val metadataLog = new FakeCompactibleFileStreamLog("vvv", spark, dir.getAbsolutePath)
+              assert(metadataLog.add(0, Array("batch0")))
+              val x = metadataLog.getLatest();
+              x
+              val y: Tuple2[Int, Int] = Tuple2(1, 2)
+              y.equals(2)
+              assert((1, "b") === (1,"b"))
+              assert(metadataLog.getLatest() === Some(0 -> Array("batch0")))
+              assert(metadataLog.get(0) === Some("batch0"))
+              assert(metadataLog.getLatest() === Some(0 -> "batch0"))
+              assert(metadataLog.get(None, Some(0)) === Array(0 -> "batch0"))
 
-      assert(metadataLog.add(1, Array("batch1")))
-      assert(metadataLog.get(0) === Some("batch0"))
-      assert(metadataLog.get(1) === Some("batch1"))
-      assert(metadataLog.getLatest() === Some(1 -> "batch1"))
-      assert(metadataLog.get(None, Some(1)) === Array(0 -> "batch0", 1 -> "batch1"))
+              assert(metadataLog.add(1, Array("batch1")))
+              assert(metadataLog.get(0) === Some("batch0"))
+              assert(metadataLog.get(1) === Some("batch1"))
+              assert(metadataLog.getLatest() === Some(1 -> "batch1"))
+              assert(metadataLog.get(None, Some(1)) === Array(0 -> "batch0", 1 -> "batch1"))
 
-      // Adding the same batch does nothing
-      metadataLog.add(1, Array("batch1-duplicated"))
-      assert(metadataLog.get(0) === Some("batch0"))
-      assert(metadataLog.get(1) === Some("batch1"))
-      assert(metadataLog.getLatest() === Some(1 -> "batch1"))
-      assert(metadataLog.get(None, Some(1)) === Array(0 -> "batch0", 1 -> "batch1"))
-    }
-  }
-  */
+              // Adding the same batch does nothing
+              metadataLog.add(1, Array("batch1-duplicated"))
+              assert(metadataLog.get(0) === Some("batch0"))
+              assert(metadataLog.get(1) === Some("batch1"))
+              assert(metadataLog.getLatest() === Some(1 -> "batch1"))
+              assert(metadataLog.get(None, Some(1)) === Array(0 -> "batch0", 1 -> "batch1"))
+            }
+          }
+          */
 /*
   testWithUninterruptibleThread(
     "HDFSMetadataLog: fallback from FileContext to FileSystem", quietly = true) {
@@ -457,6 +357,13 @@ class CompactibleFileStreamLogSuite extends SparkFunSuite with SharedSQLContext 
       fm.rename(path2, path3)
     }
   }*/
+
+  private def withFakeCompactibleFileStreamLog(f: FakeCompactibleFileStreamLog => Unit): Unit = {
+    withTempDir { file =>
+      val sinkLog = new FakeCompactibleFileStreamLog("vvv", spark, file.getCanonicalPath)
+      f(sinkLog)
+    }
+  }
 }
 
 class FakeCompactibleFileStreamLog(
